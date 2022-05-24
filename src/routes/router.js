b2b0fb1b532fecf376584a52fd63e21a44bc6bca
leoDlopez <lopez_lopez_daniel@hotmail.com>
2022-05-24 00:13:20 -0500

    Mui instalado

--- src/routes/router.js
+++ src/routes/router.js
@@ -8,13 +8,15 @@ import { Calculadora } from '../views/Calculadora';
 import { Compra } from '../views/Compra';
 import { Cuenta } from '../views/Cuenta';
 import { Documentos } from '../views/Documentos';
+import { Home } from '../views/Home';
 import { Panel } from '../views/Panel';
 
 export default function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route exact path="/" element={<Panel />} />
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/panel" element={<Panel />} />
                 <Route exact path="/compra" element={<Compra />} />
                 <Route exact path="/cuenta" element={<Cuenta />} />
                 <Route exact path="/calculadora" element={<Calculadora />} />
---
+++ src/views/Home/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './styles/style.css'
+
+export const Home = () => {
+    return (
+        <div className='container'>
+            index
+        </div>
+    )
+}
